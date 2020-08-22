bd16ee380fe63470684b8b2a74c3d1ca536f4f3b
afhernani <afhernani@gmail.com>
2020-08-22 13:55:21 +0100

    include videostream to library

--- ffpeglib/__init__.py
+++ ffpeglib/__init__.py
@@ -9,11 +9,18 @@ Modules:
 
 * __init__.py: API imports
 
+
 """
-try:
-    from .moviebox import Boxd, MovieBox
-except:
-    from moviebox import Boxd, MovieBox
 
 __author__ = 'hernani'
-__version__ = '0.0.1'
+__email__ = 'afhernani@gmail.com'
+__apply__ = 'library kvcomic'
+__version__ = '0.3'
+
+try:
+    from .moviebox import Boxd, MovieBox
+    from .videostream import VideoStream
+except:
+    from moviebox import Boxd, MovieBox
+    from videostream import VideoStream
+
